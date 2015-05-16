fca531b1cf4d3b808e69317e052d2a755b09cbae
Jose Viscasillas <jose.viscasillas@gmail.com>
2015-05-16 15:21:21 -0400

    Initial Commit

---
+++ talk/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  talk
+//
+//  Created by Jose Viscasillas on 5/16/15.
+//  Copyright (c) 2015 Jose Viscasillas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
